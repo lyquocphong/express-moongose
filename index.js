a20809d467c4b9a4a64e3f3eff21a944dced651c
Ly Quoc Phong <lyquocphong@gmail.com>
2018-05-08 10:49:43 +0300

    Add nodemon and set public and dynamic view

--- index.js
+++ index.js
@@ -3,35 +3,40 @@ var app = express();
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars')
-    .create({ defaultLayout: 'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+    .create({ defaultLayout: 'main', extname: '.hbs' });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
-
 app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
-});
+    var fortunes = [
+        "Conquer your fears or they will conquer you.",
+        "Rivers need springs.",
+        "Do not fear what you don't know.",
+        "You will have a pleasant surprise.",
+        "Whenever possible, keep it simple.",
+    ];
 
-// custom 404 page
-app.use(function (req, res) {
-    res.type('text/plain');
+    var randomFortune =
+        fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune });
+});
+// 404 catch-all handler (middleware)
+app.use(function (req, res, next) {
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
-
-// custom 500 page
+// 500 error handler (middleware)
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function () {
