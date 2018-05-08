08e2847cb936bc1a728370d2e22341940d45944a
Ly Quoc Phong <lyquocphong@gmail.com>
2018-05-08 11:02:50 +0300

    Move fortune cookie functionality to module

--- index.js
+++ index.js
@@ -15,17 +15,8 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 app.get('/about', function (req, res) {
-    var fortunes = [
-        "Conquer your fears or they will conquer you.",
-        "Rivers need springs.",
-        "Do not fear what you don't know.",
-        "You will have a pleasant surprise.",
-        "Whenever possible, keep it simple.",
-    ];
-
-    var randomFortune =
-        fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    var fortune = require('./lib/fortune.js');
+    res.render('about', { fortune: fortune.getFortune() } );
 });
 // 404 catch-all handler (middleware)
 app.use(function (req, res, next) {
